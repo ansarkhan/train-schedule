677d1d910a698a4cb3646cf5ee81f006b5c47cd6
ansarkhan <ansarkhan@me.com>
2019-02-01 08:17:59 -0500

    adding trains

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,13 +13,94 @@ var database = firebase.database();
 
 // create DOM elements
 
-var DOMelements = {
-    playerName: '#player-name',
-    playerTwoName: '#player-2-name',
-    checkIn: '#check-in',
-    checkInTwo: '#check-in-2',
-    playerOneChoice: '#player-1-choice',
-    playerTwoChoice: '#player-2-choice',
-    play: '#play',
+// var DOMelements = {
+//     playerName: '#player-name',
+//     playerTwoName: '#player-2-name',
+//     checkIn: '#check-in',
+//     checkInTwo: '#check-in-2',
+//     playerOneChoice: '#player-1-choice',
+//     playerTwoChoice: '#player-2-choice',
+//     play: '#play',
+
+// }
+
+var addTrain = function() {
+    name = $('#name').val().trim();
+    destination = $('#destination').val().trim();
+    firstTrain = $('#first-train').val().trim();
+    frequency = $('#frequency').val().trim();
+
+    var train = {
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    };
+
+
+    var newRow = 
+        $("<tr>").append(
+        $("<td>").text(name),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text('next arrival'),
+        $("<td>").text('minutes away')
+        );
+    
+      $("#train-table > tbody").append(newRow);
+
+      database.ref().push(train);
+
 
 }
+
+
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+    addTrain();
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  });
+
+// database.ref().on("child_added", function(childSnapshot) {
+//     console.log(childSnapshot.val());
+  
+//     // Store everything into a variable.
+//     var empName = childSnapshot.val().name;
+//     var empRole = childSnapshot.val().role;
+//     var empStart = childSnapshot.val().start;
+//     var empRate = childSnapshot.val().rate;
+  
+//     // Employee Info
+//     console.log(empName);
+//     console.log(empRole);
+//     console.log(empStart);
+//     console.log(empRate);
+  
+//     // Prettify the employee start
+//     var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+  
+//     // Calculate the months worked using hardcore math
+//     // To calculate the months worked
+//     var empMonths = moment().diff(moment(empStart, "X"), "months");
+//     console.log(empMonths);
+  
+//     // Calculate the total billed rate
+//     var empBilled = empMonths * empRate;
+//     console.log(empBilled);
+  
+//     // Create the new row
+//     var newRow = $("<tr>").append(
+//       $("<td>").text(empName),
+//       $("<td>").text(empRole),
+//       $("<td>").text(empStartPretty),
+//       $("<td>").text(empMonths),
+//       $("<td>").text(empRate),
+//       $("<td>").text(empBilled)
+//     );
+  
+//     // Append the new row to the table
+//     $("#employee-table > tbody").append(newRow);
+//   });
