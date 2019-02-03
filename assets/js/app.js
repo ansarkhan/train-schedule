270aa847a068be29da2e360fd458f1c1d2c30ded
ansarkhan <ansarkhan@me.com>
2019-02-03 12:10:21 -0500

    some progress on minutes away

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,22 +11,6 @@ firebase.initializeApp(config);
 
 var database = firebase.database(); 
 
-// create DOM elements
-
-// var DOMelements = {
-//     playerName: '#player-name',
-//     playerTwoName: '#player-2-name',
-//     checkIn: '#check-in',
-//     checkInTwo: '#check-in-2',
-//     playerOneChoice: '#player-1-choice',
-//     playerTwoChoice: '#player-2-choice',
-//     play: '#play',
-
-// }
-
-var nextArrival = null;
-
-
 
 var addTrain = function() {
     name = $('#name').val().trim();
@@ -69,28 +53,27 @@ database.ref('/trains').on("child_added", function(childSnapshot) {
     var firstTrain = childSnapshot.val().firstTrain;
     var frequency = childSnapshot.val().frequency;
 
+    var nextArrival = null;
+    var minutesAway = null;
     // Next arrival
 
+    // var calcArrival = function() {
+    //     // for loop
+    //         // for firstTrain + frequency > current time
+    //         // store that time in DB
+
+    // }
 
     // Minutes away
+    // current time - next arrival
+    var m = moment(new Date()).format("hh:mm");
+    var initTime = moment(firstTrain, 'HH:mm');
+    var duration = moment.duration(m.diff(initTime));
+    diffy = duration % frequency;
+    minsAway = frequency - diffy;
+
   
-    // Train Info
-    // console.log(name);
-    // console.log(destination);
-    // console.log(firstTrain);
-    // console.log(frequency);
   
-    // Prettify the employee start
-    // var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
-  
-    // Calculate the months worked using hardcore math
-    // To calculate the months worked
-    // var empMonths = moment().diff(moment(empStart, "X"), "months");
-    // console.log(empMonths);
-  
-    // Calculate the total billed rate
-    // var empBilled = empMonths * empRate;
-    // console.log(empBilled);
   
     // Create the new row
     var newRow = 
@@ -98,10 +81,48 @@ database.ref('/trains').on("child_added", function(childSnapshot) {
     $("<td>").text(name),
     $("<td>").text(destination),
     $("<td>").text(frequency),
-    $("<td>").text('next arrival'), // add next arrival
+    $("<td>").text(minsAway), // add next arrival
     $("<td>").text('minutes away') // add minutes away
     );
   
     // Append the new row to the table
     $("#train-table > tbody").append(newRow);
   });
+
+//   var m = moment(new Date());
+//   console.log(m);
+
+//   var hoursMins = m.format("hh:mm")
+//   console.log(hoursMins);
+
+// var m = moment(new Date()).format("hh:mm");
+// console.log(m);
+
+// initTime = "11:35"
+// frequencyMin = 15;
+
+
+// var calcArrival = function() {
+//     var t = moment(initTime, 'HH:mm');
+//     var duration = moment.duration(m.diff(t));
+//     var minutes = duration.asMinutes();
+//     console.log(minutes);
+//     diffe = minutes % frequencyMin;
+//     timeL = frequencyMin - diffe;
+//     console.log('time till next train: ' + timeL);
+
+
+// }
+
+// calcArrival();
+
+
+
+//   var convertedDate = moment(randomDate, randomFormat);
+
+  // Using scripts from moment.js write code below to complete each of the following.
+  // Console.log to confirm the code changes you made worked.
+
+  // 1 ...to convert the randomDate into three other date formats
+  // console.log(convertedDate.format("MM/DD/YY"));
+  // console.log(convertedDate.format("MMM Do, YYYY hh:mm:ss"));
